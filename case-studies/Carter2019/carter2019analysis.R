8ba2aac7ca18c953481e8184ca935afb3c8133de
SamCH93 <samuel.pawel@gmail.com>
2024-09-04 13:54:45 +0200

    increase legend text size

--- case-studies/Carter2019/carter2019analysis.R
+++ case-studies/Carter2019/carter2019analysis.R
@@ -114,7 +114,8 @@ ggplot(data = carter2019summarieslong,
   theme_bw() +
   theme(panel.grid.minor = element_blank(),
         panel.grid.major.y = element_blank(),
-        legend.position = "bottom")
+        legend.position = "bottom",
+        legend.text = element_text(size = rel(1.025)))
 ggsave("fig-carter-handling-missingness.pdf", width = 8, height = 5, scale = 0.9)
 
 ## visualize missingness
@@ -207,3 +208,40 @@ plotC <- ggdraw(add_sub(plotC1, bquote("Condition-wise non-convergence rate"),
 plot_grid(plotA, plotC, ncol = 1, rel_heights = c(1, 4), labels = c("A", "B"))
 ggsave(filename = "fig-carter-exploring-missingness.pdf", width = 8.5,
        height = 10.5, scale = 1.025)
+
+sessionInfo()
+## R version 4.4.1 (2024-06-14)
+## Platform: x86_64-pc-linux-gnu
+## Running under: Ubuntu 22.04.4 LTS
+
+## Matrix products: default
+## BLAS:   /usr/lib/x86_64-linux-gnu/blas/libblas.so.3.10.0
+## LAPACK: /usr/lib/x86_64-linux-gnu/lapack/liblapack.so.3.10.0
+
+## locale:
+##  [1] LC_CTYPE=en_US.UTF-8       LC_NUMERIC=C
+##  [3] LC_TIME=de_CH.UTF-8        LC_COLLATE=en_US.UTF-8
+##  [5] LC_MONETARY=de_CH.UTF-8    LC_MESSAGES=en_US.UTF-8
+##  [7] LC_PAPER=de_CH.UTF-8       LC_NAME=C
+##  [9] LC_ADDRESS=C               LC_TELEPHONE=C
+## [11] LC_MEASUREMENT=de_CH.UTF-8 LC_IDENTIFICATION=C
+
+## time zone: Europe/Zurich
+## tzcode source: system (glibc)
+
+## attached base packages:
+## [1] stats     graphics  grDevices utils     datasets  methods   base
+
+## other attached packages:
+## [1] cowplot_1.1.3    ggbeeswarm_0.7.2 ggplot2_3.5.1    tidyr_1.3.1
+## [5] dplyr_1.1.4
+
+## loaded via a namespace (and not attached):
+##  [1] crayon_1.5.3      vctrs_0.6.5       cli_3.6.3         rlang_1.1.4
+##  [5] purrr_1.0.2       generics_0.1.3    textshaping_0.4.0 labeling_0.4.3
+##  [9] glue_1.7.0        colorspace_2.1-1  ragg_1.3.2        scales_1.3.0
+## [13] fansi_1.0.6       grid_4.4.1        munsell_0.5.1     tibble_3.2.1
+## [17] lifecycle_1.0.4   compiler_4.4.1    vipor_0.4.7       pkgconfig_2.0.3
+## [21] beeswarm_0.4.0    systemfonts_1.1.0 farver_2.1.2      R6_2.5.1
+## [25] tidyselect_1.2.1  utf8_1.2.4        pillar_1.9.0      magrittr_2.0.3
+## [29] tools_4.4.1       withr_3.0.1       gtable_0.3.5
